45669fae37ad680c98616a9bc81598a1edf6b919
micheal okosun <michealokosun6@gmail.com>
2022-09-09 01:48:11 +0000

    size of byte

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char charType;
+	int inttype;
+	long longType;
+	long long longerType;
+	float floatType;
+
+
+	printf("size of a char: %zu byte (s)", sizeof(charType));
+	printf("Size of an int: %zu byte(s)", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)", sizeof(lontType));
+	printf("Size of a long long int : %zu byte(s)", sizeof(longerType));
+	printf("Size of an int: %zu byte(s)", sizeof(floatType))
+
+	return (0);
+
+
+
+}
