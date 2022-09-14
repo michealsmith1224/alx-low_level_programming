21a38c104500581bf714d6d6f68f2470306463f0
micheal okosun <michealokosun6@gmail.com>
2022-09-14 17:20:24 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * _abs - Entry point
+  *
+  * Return: void
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+
+	return (0);
+}
