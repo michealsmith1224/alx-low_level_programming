cd00feec02d88a974b2135b2aba2be266558c753
micheal okosun <michealokosun6@gmail.com>
2022-09-15 04:09:21 +0000

    mike

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,30 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n > 98)
 	{
-		for (n = n; n <= 97; n++)
+		while (n >= 98)
+		{
 			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
+			n--;
+		}
 	}
 	else
 	{
-		for (n = n; n > 98; n--)
-			printf("%d, ", n);
-
-		printf("98\n");
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
+			n++;
+		}
 	}
+	printf("\n");
 }
 
