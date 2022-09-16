d4845b5da8e1703825b569c61ea0b28c9152311a
micheal okosun <michealokosun6@gmail.com>
2022-09-16 04:53:06 +0000

    prine

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,48 +10,19 @@
   * Return: 0
 */
 
-unsigned int long maxPrimeFactors(unsigned int long n)
-{
-	unsigned int long maxPrime = -1;
-	int i;
-
-	while (n % 2 == 0)
-	{
-		maxPrime = 2;
-		n >>= 1;
-	}
-	while (n % 3 == 0)
-	{
-		maxPrime = 3;
-		n = n / 3;
-	}
-
-	for (i = 5; i <= sqrt(n); i += 6)
-	{
-		while (n % i == 0)
-		{
-			maxPrime = i;
-			n = n / i;
-		}
-		while (n % (i + 2) == 0)
-		{
-			maxPrime = i + 2;
-			n = n / (i + 2);
-		}
-	}
-
-	if (n > 4)
-		maxPrime = n;
-	return (maxPrime);
-
-}
-
-
-
-
 int main(void)
 {
+	unsigned int long n = 612852475143, a = (int) sqrt(n);
 
-	printf("%lu \n", maxPrimeFactors(612852475143));
+	while (1)
+	{
+		if (n % a == 0)
+		{
+			printf("%lu \n", n / a);
+			break;
+		}
+		a--;
+	}
+
 	return (0);
 }
