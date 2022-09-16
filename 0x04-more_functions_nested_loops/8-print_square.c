3e9539f373de94df27cb114d7e6642c3fa33c144
micheal okosun <michealokosun6@gmail.com>
2022-09-16 03:06:45 +0000

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_square - print square
+  *
+  * Return: 0
+*/
+
+void print_square( int size)
+{
+	int i = 0, cc;
+
+	while (i < size && size > 0)
+	{
+		cc = 0;
+
+		while (cc < size)
+		{
+			_putchar('#');
+			cc++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
