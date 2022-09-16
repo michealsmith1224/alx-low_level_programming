930fbebdd855dc8e187b53660dc18ee76032f35d
micheal okosun <michealokosun6@gmail.com>
2022-09-16 06:23:07 +0000

    debug

--- 0-main.c
+++ 0-main.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
   * main - Entry point
   *
   * Return: 0
 */
-
 
 int main(void)
 {
@@ -14,6 +12,5 @@ int main(void)
 	 i = 0;
 
 	 positive_or_negative(i);
-
 	 return (0);
 }
