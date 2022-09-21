f6f7707c3f71b69cfcc179d8cc4f45492d539c4f
micheal okosun <michealokosun6@gmail.com>
2022-09-21 07:38:52 +0000

    pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * swap_int - swap two integer
+  *
+  * @a: first para
+  * @b: second para
+  *
+  * Return: 0
+*/
+
+
+void swap_int(int *a, int *b)
+{
+	*a = 402;
+	*b = 300;
+}
