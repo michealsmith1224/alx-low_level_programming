8a8d48da133556bdb2a3a8e3a407f5204a961c4f
micheal okosun <michealokosun6@gmail.com>
2022-09-22 10:16:19 +0000

    pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _puts - print string
+  * @str: para
+  *
+  * Return: void
+*/
+
+
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
