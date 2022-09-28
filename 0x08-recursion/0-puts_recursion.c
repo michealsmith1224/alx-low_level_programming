2e48151ce25ec8201d1cb322a6c262e25bfef2c3
micheal okosun <michealokosun6@gmail.com>
2022-09-28 17:02:20 +0000

    recur

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+  * _puts_recursion - print string
+  * @s: para
+  *
+  * Return 0
+
+*/
+
+
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	_putchar('\n');
+}
