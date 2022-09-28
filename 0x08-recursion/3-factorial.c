779ee8d20c38de2e070167008698a8eb0cd11d93
micheal okosun <michealokosun6@gmail.com>
2022-09-28 20:20:04 +0000

    precur

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,12 +12,10 @@ int factorial(int n)
 	if (n < 0)
 	{
 		return (-1);
-	}else if (n == 0)
+	} else if (n == 0)
 	{
 		return (1);
-	}else
-	{
-		return (n * factorial(n - 1));
 	}
+	return (n * factorial(n - 1));
 }
 
