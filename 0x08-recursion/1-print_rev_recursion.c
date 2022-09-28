f96f91736dada6f45dd27907f8937169fee9450e
micheal okosun <michealokosun6@gmail.com>
2022-09-28 18:19:35 +0000

    precur

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+  * _print_rev_recursion - reverse string
+  * @s: pars
+  *
+  * Return: 0
+*/
+
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
