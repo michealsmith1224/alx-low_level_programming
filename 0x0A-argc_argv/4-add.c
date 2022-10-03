13ab445f7cfe69c3e4e779eda7dcb50f6a66695e
micheal okosun <michealokosun6@gmail.com>
2022-10-03 14:19:58 +0000

    argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+/**
+  * check_num - check for digit
+  *
+  * @str: para 1
+  *
+  * Return: 1
+*/
+
+int check_num(char *str)
+{
+	unsigned int i;
+
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		if (!isdigit(str[i]))
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
+
+
+
+/**
+  * main - adds positive numbers.
+  * @argc: para 1
+  * @argv: para 2
+  *
+  * Return: 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+	int j;
+	int str_int;
+
+	int sum = 0;
+
+	j = 1;
+
+	while (j < argc)
+	{
+		if (check_num(argv[j]))
+		{
+			str_int = atoi(argv[j]);
+			sum += str_int;
+		} else
+		{
+			printf("Error");
+			return (1);
+		}
+		j++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+
+}
