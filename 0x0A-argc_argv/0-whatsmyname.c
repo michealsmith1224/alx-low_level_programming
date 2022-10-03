ae815bdb9f84198229817d06b0a8ce892e3aa57e
micheal okosun <michealokosun6@gmail.com>
2022-10-03 10:38:10 +0000

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - priint programe name
+  *
+  * @argc: interger
+  * @argv: arrays
+  *
+  * Return: 0
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
