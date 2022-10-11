be08a098fadb4ea72291f6cfab0c16bbebfdaca5
micheal okosun <michealokosun6@gmail.com>
2022-10-11 09:40:23 +0000

    struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "doh.h"
+
+/**
+  * init_dog - functio nfor dog
+  * @d: the pointer
+  * @name: the name
+  * @age: the age
+  * @owner: the owner
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
