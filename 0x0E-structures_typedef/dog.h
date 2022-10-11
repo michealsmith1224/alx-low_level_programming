65c8600472ba3154a9018dfc32fb671385b46807
micheal okosun <michealokosun6@gmail.com>
2022-10-11 09:02:07 +0000

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+  * struct dog - structure for dog
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: the owner
+*/
+
 struct dog
 {
 	char *name;
 	double age;
 	char *owner;
 };
+
+#endif
