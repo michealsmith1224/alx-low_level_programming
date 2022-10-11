cf5e91368c231c0d798c9fafddc6506392bb1176
micheal okosun <michealokosun6@gmail.com>
2022-10-11 10:38:58 +0000

    struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,43 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+  * print_dog - output dog details
+  * @d: para 1
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (!(d->name))
+	{
+		printf("Name: nill\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+
+	if (!(d->age))
+	{
+		printf("Age: nill");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+
+	if (!(d->owner))
+	{
+		printf("Owner: nill");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+
+
+
+}
