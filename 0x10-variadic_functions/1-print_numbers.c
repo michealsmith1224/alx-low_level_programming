0ec1a8afcf8dc1bf8d93203c9d9a05c63d961078
micheal okosun <michealokosun6@gmail.com>
2022-10-17 11:53:30 +0000

    variadic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,3 +9,33 @@
   * @n: cont number
 */
 
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, b;
+	va_list other_num;
+
+
+	if (separator == NULL || n == 0)
+		return;
+
+	va_start(other_num, n);
+
+	i = 0;
+	while (i < n)
+	{
+		b = va_arg(other_num, int);
+		printf("%d", b);
+
+		if (i == n - 1)
+			break;
+
+		printf("%s ", separator);
+
+		i++;
+	}
+
+	printf("\n");
+}
+
+
+
