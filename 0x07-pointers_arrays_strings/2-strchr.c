7c1947b46bae83042e860baef015f8a4b83fc314
micheal okosun <michealokosun6@gmail.com>
2022-10-19 09:07:55 +0000

    more func

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <string.h>
-
+#include <stddef.h>
 /**
   * _strchr - Write a function that locates a character in a string.
   * @s: the string
@@ -11,15 +10,24 @@
 
 char *_strchr(char *s, char c)
 {
+	int a = 0, b;
+
+
 	if (s == NULL)
-		return NULL;
-	while (*s)
+		return (NULL);
+
+	while (s[a])
+		a++;
+
+	for (b = 0; b < a; b++)
 	{
-		if (*s == c)
-			return (s);
-		s++;
+		if (c == s[b])
+		{
+			return (s + b);
+		}
 	}
 
-	return NULL;
+
+	return ('\0');
 
 }
