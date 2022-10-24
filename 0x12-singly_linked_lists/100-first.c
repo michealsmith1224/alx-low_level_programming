a6d23bad4496fafe21bb3f25720d71bd49ba75bd
micheal okosun <michealokosun6@gmail.com>
2022-10-24 17:27:08 +0000

    list2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@
 */
 
 
-void __attribute__((constructor)) first_run(void)
+void __attribute__((constructor)) first_run()
 {
 	printf(
 			"you're beat! and yet, you must allow,\nI bore my houseupon my back!\n");
