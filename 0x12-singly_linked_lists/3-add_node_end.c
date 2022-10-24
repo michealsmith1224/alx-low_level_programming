ce804a46201f52cb0c132d45eac98a2c2e30ea11
micheal okosun <michealokosun6@gmail.com>
2022-10-24 15:50:47 +0000

    list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * add_node - add to the end
+  * add_node_end - add to the end
   * @head: begining of node
   * @str: para passed
   * Return: 0
@@ -18,26 +18,26 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (0);
 
 	newnode->str = strdup(str);
+	newnode->len = strlen(str);
 
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
-		newnode->next = NULL;
 		*head = newnode;
 	}
 	else
 	{
 		temp = *head;
+
 		while (temp->next != NULL)
 		{
 			temp = temp->next;
 		}
 
-		newnode->next = NULL;
 		temp->next = newnode;
 	}
 
-	return (*head);
+	return (temp->next);
 }
 
 
