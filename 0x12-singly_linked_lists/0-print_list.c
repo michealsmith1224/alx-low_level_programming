7f47ec10caf3e8a6c71866d3d164762bc2480229
micheal okosun <michealokosun6@gmail.com>
2022-10-24 10:24:39 +0000

    list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,30 +8,27 @@
 */
 
 
-size_t print_list(list_t *h)
+size_t print_list(const list_t *h)
 {
-	list_t *temp;
 	size_t count;
-
-	temp = h;
 
 	count = 0;
 
 
-	while (temp->next != NULL)
+	while (h)
 	{
-		if (temp->str == NULL)
+		if (h->str == NULL)
 		{
-			temp->str = "(nil)";
-			temp->len = 0;
+			printf("[%d] %s\n", 0, "(nil)");
 		}
-		printf("[%d] %s\n", temp->len, temp->str);
-		temp = temp->next;
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
 		count++;
 	}
-
-	printf("[%d] %s\n", temp->len, temp->str);
-	count++;
 
 
 	return (count);
