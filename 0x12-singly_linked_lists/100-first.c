b1efc85c10ea3a45cdd73b9b75f7081eb31c88a7
micheal okosun <michealokosun6@gmail.com>
2022-10-24 16:44:39 +0000

    list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+  * first_run - run before main
+  *
+  * Return 9
+*/
+
+
+void first_run(void)
+{
+	printf("you're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
