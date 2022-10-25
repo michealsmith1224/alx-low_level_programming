977eb32ab093835a957de16174f370b746ad2769
micheal okosun <michealokosun6@gmail.com>
2022-10-25 22:57:13 +0000

    more list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+
+/**
+  * insert_nodeint_at_index - insert node at given position
+  * @head: start node
+  * @idx: position to inser
+  * @n: n value
+  * Return: 0
+*/
+
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp, *nodelen;
+	unsigned int count, i;
+
+	nodelen = *head;
+	count = 0;
+	while (nodelen)
+	{
+		nodelen = nodelen->next;
+		count++;
+	}
+
+	printf("the len is %d\n", count);
+
+
+
+	if (idx > count)
+	{
+		return (NULL);
+	}
+	else
+	{
+		listint_t *newnode;
+
+		newnode = (listint_t *)malloc(sizeof(listint_t));
+
+		if (newnode == NULL)
+			return (NULL);
+
+		newnode->n = n;
+
+		temp = *head;
+		i = 1;
+		while (i < idx)
+		{
+			temp = temp->next;
+			i++;
+		}
+
+		newnode->next = temp->next;
+		temp->next = newnode;
+
+	}
+
+	return (*head);
+
+
+}
+
