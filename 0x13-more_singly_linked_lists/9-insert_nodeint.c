0949a68e70ca6e08a5ae35e35f729b2d323d884d
micheal okosun <michealokosun6@gmail.com>
2022-10-25 23:03:32 +0000

    more list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		newnode->n = n;
 		temp = *head;
-		i = 1;
+		i = 0;
 		while (i < idx)
 		{
 			temp = temp->next;
