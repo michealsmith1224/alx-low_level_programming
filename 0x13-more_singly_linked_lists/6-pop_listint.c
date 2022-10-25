1355e8d6b2bc39d752f2a143712e1fd4708374c6
micheal okosun <michealokosun6@gmail.com>
2022-10-25 07:35:27 +0000

    more list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,28 @@
 #include "lists.h"
 
+/**
+  * pop_listint - remove head node
+  * @head: head node
+  * Return: 0
+*/
+
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	n =(*head)->n;
+	*head = (*head)->next;
+	free(temp);
+
+
+	return (n);
+}
 
 
