c0e6c2274fc36eb748885fd954379c83ede4ef77
micheal okosun <michealokosun6@gmail.com>
2022-10-25 07:14:29 +0000

    more list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -16,13 +16,12 @@ void free_listint2(listint_t **head)
 		return;
 
 
-	temp = *head;
 
 	while (*head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
 
 	head = NULL;
